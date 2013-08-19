b6e6e1dd7e4091203febe0bcb69d393b96437e3a
Kenji Fujiwara <kenji-f@is.naist.jp>
2013-08-20 05:12:04 +0900

    added initialization script for neobundle.vim

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ ln -i -h -s $PWD/tmux.conf ~/.tmux.conf
 
 ln -i -h -s $PWD/vimrc ~/.vimrc
 ln -i -h -s $PWD/vim ~/.vim
+
+if [ ! -e "${PWD}/vim/bundle/neobundle.vim" ] ; then
+	echo "clone neobundle.vim"
+	git clone https://github.com/Shougo/neobundle.vim.git vim/bundle/neobundle.vim
+fi
