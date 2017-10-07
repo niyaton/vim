25fd1a2958b4fb6eda77ff593b7b6fb5164e2852
Kenji Fujiwara <niyaton@gmail.com>
2017-10-07 22:06:19 +0900

    no longer use neobundle

--- setup.sh
+++ setup.sh
@@ -1,7 +1,2 @@
 ln -i -n -s $PWD/vimrc ~/.vimrc
 ln -i -n -s $PWD/vim ~/.vim
-
-if [ ! -e "${PWD}/vim/bundle/neobundle.vim" ] ; then
-	echo "clone neobundle.vim"
-	git clone https://github.com/Shougo/neobundle.vim.git vim/bundle/neobundle.vim
-fi
