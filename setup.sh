11356de5b63d4e5a0125f328c44ea86cf07969a3
Kenji Fujiwara <niyaton@gmail.com>
2017-11-28 16:13:47 +0900

    fix setup.sh for neovim

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ vim +PlugInstall +qall
 
 if which nvim &> /dev/null
 then
+	mkdir -p ~/.config
 	ln -i -n -s $PWD/vim ~/.config/nvim
 	ln -i -n -s $PWD/vimrc ~/.config/nvim/init.vim
 	nvim +PlugInstall +qall
